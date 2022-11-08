6ac07294f95f77ebbb8fb03b2ff8c6fa8a0d1809
Jesperere <jesper_sylte@msn.com>
2022-11-08 11:37:32 +0100

    mongoose + apollo setup

---
+++ server.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './src/graphql/typeDefs';
+import { resolvers } from './src/graphql/resolvers';
+
+dotenv.config()
+
+const uri = process.env.MONGO_URL
+console.log(uri);
+
+const port = process.env.PORT
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+mongoose.connect(uri)
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen(port)
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
