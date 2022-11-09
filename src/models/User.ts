26208ac893d4322c2158196359c8931920e1ff76
Jesperere <jesper_sylte@msn.com>
2022-11-09 12:08:44 +0100

    updated user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,10 @@
-import mongoose, {Schema} from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
 
 export const userSchema = new Schema({
     username: { type: String },
     email: { type: String, unique: true },
     password: { type: String },
+    color: { type: String },
     token: { type: String }
 })
 
